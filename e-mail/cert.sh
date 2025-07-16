a16f64a08934e0c7cacf6dc7db054b2c2b934ccc
Juris Embergs <embergs.usa@gmail.com>
2025-07-16 10:50:12 +0300

    Update cert.sh

--- e-mail/cert.sh
+++ e-mail/cert.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 RECIPIENT_EMAIL="<---E-PASTS--PIRMAIS--->"
 EMAIL_SECONDARY="<---E-PASTS---OTRAIS---(XXX@sms.lmt.lv)--->"
 SENDER_NAME="CERT.LV Notifier"
@@ -15,11 +14,18 @@ UZ_EMAIL_LOG="$LOG_DIR/uz-email.log"
 TRANSFORM_LOG="$LOG_DIR/transform.log"
 VAR_LOG_CERT="/var/log/cert.log"
 
-wget https://cert.lv/lv/rss/incidenti/bridinajumi.xml -O "$DATNE"
+CACHET_API_URL="https://sesks.lu.lv/api/v1/incidents"
+CACHET_API_TOKEN="<---CACHET--TOKENS--->"
+PUSHBULLET_API_URL="https://api.pushbullet.com/v2/pushes"
+PUSHBULLET_ACCESS_TOKEN="<---PUSHBULLET--TOKENS--->"
+CERT_LV_RSS_FEED="https://cert.lv/lv/rss/incidenti/bridinajumi.xml"
+CERT_LV_INFO_URL="https://cert.lv/lv/incidenti/bridinajumi"
+
+wget "$CERT_LV_RSS_FEED" -O "$DATNE"
 
 cat "$DATNE" | grep -E '(title>|pubDate>|description>)' | \
-sed -e 's/^[ \t]*//' -e 's/<title>/\n/' -e 's/<\/title>//' -e 's/<pubDate>//' -e 's/<\/pubDate>//' -e 's/<description>//' -e 's/<\/description>//' | \
-perl -p -e 's/&.* ?;//g' | sed '1,4d' > "$NEW"
+sed -e 's/^[ \t]*//' -e 's/<pubDate>//' -e 's/<\/pubDate>//' -e 's/<description>//' -e 's/<\/description>//' | \
+perl -p -e 's/&.* ?;//g' | sed '1,3d' > "$NEW"
 
 if diff <(sort "$NEW") <(sort "$OLD"); then
     echo "CERT.LV brīdinājumi nemainīgi, pārbaudīts plkst. $(LANG='lv_LV.UTF-8'; date)" >> "$VAR_LOG_CERT"
@@ -27,30 +33,35 @@ if diff <(sort "$NEW") <(sort "$OLD"); then
     rm -f *.log
 else
     if [ "$(cat "$NEW" | wc -l)" -ge "2" ]; then
-        echo "CERT brīdina! Jauni brīdinājumi https://cert.lv/lv/incidenti/bridinajumi. Pārbaudīts plkst. $(LANG='lv_LV.UTF-8'; date)" >> "$VAR_LOG_CERT"
+        echo "CERT brīdina! Jauni brīdinājumi $CERT_LV_INFO_URL. Pārbaudīts plkst. $(LANG='lv_LV.UTF-8'; date)" >> "$VAR_LOG_CERT"
 
         diff <(sort "$NEW") <(sort "$OLD") > "$IZMAINAS_LOG"
         comm -23 <(sort -u "$NEW") <(sort -u "$OLD") > "$TEKSTS"
-
-        cat "$TEKSTS" | awk 'NR == 3' > "$TRANSFORM_LOG"
-        cat "$TEKSTS" | awk 'NR == 2' >> "$TRANSFORM_LOG"
-        cat "$TEKSTS" | awk 'NR == 1' >> "$TRANSFORM_LOG"
+        cat "$TEKSTS" | grep title | sed -e 's/<title>/\n/' -e 's/<\/title>//' > "$TRANSFORM_LOG"
+        cat "$TEKSTS" | grep -v title >> "$TRANSFORM_LOG"
         cat "$TRANSFORM_LOG" > "$TEKSTS"
 
         cat "$TEKSTS" | grep -v "$(date +"%a")" > "$UZ_EMAIL_LOG"
 
         if [ "$(cat "$UZ_EMAIL_LOG" | wc -l)" -ge "1" ]; then
             MESSAGE_CLEAN=$(while read -r LINE; do echo "$LINE"; done < "$UZ_EMAIL_LOG")
-            EMAIL_BODY="CERT brīdina!\n\n$MESSAGE_CLEAN\n\nPlašāka informācija: https://cert.lv/lv/incidenti/bridinajumi"
+            EMAIL_BODY="CERT brīdina!\n\n$MESSAGE_CLEAN\n\nPlašāka informācija: $CERT_LV_INFO_URL"
             MESSAGE_CLEAN_ESCAPED=$(echo "$MESSAGE_CLEAN" | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/"/\\"/g')
-            EMAIL_BODY_JSON="CERT brīdina!\\n\\n${MESSAGE_CLEAN_ESCAPED}\\n\\nPlašāka informācija: https://cert.lv/lv/incidenti/bridinajumi"
-            # Send email
+            EMAIL_BODY_JSON="CERT brīdina!\\n\\n${MESSAGE_CLEAN_ESCAPED}\\n\\nPlašāka informācija: $CERT_LV_INFO_URL"
+            
             echo -e "$EMAIL_BODY" | mail -s "$EMAIL_SUBJECT" -r "$SENDER_NAME <$RECIPIENT_EMAIL>" "$RECIPIENT_EMAIL"
             echo -e "CERT.LV ALERT!" | mail -s "[ALERT] CERT.LV ALERT" "$EMAIL_SECONDARY"
-            # curl -X POST 'https://XXX.XXX.XXX/api/v1/incidents' \
-            # -H 'Content-Type: application/json' \
-            # -H 'X-Cachet-Token: <---CACHET-TOKENS--->' \
-            # -d "{\"name\":\"CERT.LV ISSUED AN ALERT!\",\"message\":\"${EMAIL_BODY_JSON}\",\"status\":2,\"visible\":1,\"component_id\":1,\"component_status\":2,\"notify\":\"false\",\"template\":\"GeneralIncidentTemplate\",\"vars\": {\"foo\":\"uno\", \"bar\":\"dos\", \"xyzzy\":\"tres\"}}"
+            
+            curl -X POST "$CACHET_API_URL" \
+            -H 'Content-Type: application/json' \
+            -H "X-Cachet-Token: $CACHET_API_TOKEN" \
+            -d "{\"name\":\"CERT.LV ISSUED AN ALERT!\",\"message\":\"${EMAIL_BODY_JSON}\",\"status\":2,\"visible\":1,\"component_id\":1,\"component_status\":2,\"notify\":\"false\",\"template\":\"GeneralIncidentTemplate\",\"vars\": {\"foo\":\"uno\", \"bar\":\"dos\", \"xyzzy\":\"tres\"}}"
+
+            curl -X POST "$PUSHBULLET_API_URL" \
+            -H "Access-Token: $PUSHBULLET_ACCESS_TOKEN" \
+            -H 'Content-Type: application/json' \
+            -d '{"type": "note", "title": "CERT ALERT", "body": "CERT ALERT"}' -k &>/dev/null
+
             if [ $? -eq 0 ]; then
                 echo "Email sent successfully to $RECIPIENT_EMAIL." >> "$VAR_LOG_CERT"
             else
